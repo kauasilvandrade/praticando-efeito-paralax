2618fd8fc2b9e046d68c18411d980408563001bd
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-12 15:48:13 -0300

    corrigindo o script do menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const btnOpen = document.querySelector('.btn-open');
+
 const hamburguer = document.querySelector('.hamburguer')
 const Menu = document.querySelector('.listaDeLinks');
 const inputBusca = document.querySelector('.input-busca');
-const linkMenu = document.querySelectorAll('.link');
+const linkMenu1 = document.querySelector('.link1');
+const linkMenu2 = document.querySelector('.link2');
+const linkMenu3 = document.querySelector('.link3');
+const linkMenu4 = document.querySelector('.link4');
+const linkMenu5 = document.querySelector('.link5');
+const linkMenu6 = document.querySelector('.link6');
 
 function mudouTamanho() {
     if (window.innerWidth <= 968) {
@@ -18,7 +24,37 @@ btnOpen.addEventListener('click', () => {
     inputBusca.classList.toggle('ativado');
 })
 
-linkMenu.addEventListener('click', () => {
+linkMenu1.addEventListener('click', () => {
+    hamburguer.classList.remove('ativado');
+    Menu.classList.remove('ativado');
+    inputBusca.classList.remove('ativado');
+})
+
+linkMenu2.addEventListener('click', () => {
+    hamburguer.classList.remove('ativado');
+    Menu.classList.remove('ativado');
+    inputBusca.classList.remove('ativado');
+})
+
+linkMenu3.addEventListener('click', () => {
+    hamburguer.classList.remove('ativado');
+    Menu.classList.remove('ativado');
+    inputBusca.classList.remove('ativado');
+})
+
+linkMenu4.addEventListener('click', () => {
+    hamburguer.classList.remove('ativado');
+    Menu.classList.remove('ativado');
+    inputBusca.classList.remove('ativado');
+})
+
+linkMenu5.addEventListener('click', () => {
+    hamburguer.classList.remove('ativado');
+    Menu.classList.remove('ativado');
+    inputBusca.classList.remove('ativado');
+})
+
+linkMenu6.addEventListener('click', () => {
     hamburguer.classList.remove('ativado');
     Menu.classList.remove('ativado');
     inputBusca.classList.remove('ativado');
