651a2cabfac519f0aee93650e8a3f793a495c210
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-11 21:57:07 -0300

    adicionando o botão de fechar o menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 const btnOpen = document.querySelector('.btn-open');
+const hamburguer = document.querySelector('.hamburguer')
 const Menu = document.querySelector('.listaDeLinks');
-const inputBusca = document.querySelector('.input-box');
+const inputBusca = document.querySelector('.input-busca');
+const linkMenu = document.querySelector('.link');
+
+function mudouTamanho() {
+    if (window.innerWidth <= 968) {
+        hamburguer.classList.remove('ativado');
+        Menu.classList.remove('ativado');
+        inputBusca.classList.remove('ativado');
+    }
+}
 
 btnOpen.addEventListener('click', () => {
+    hamburguer.classList.toggle('ativado');
     Menu.classList.toggle('ativado');
-    btnClose.classList.toggle('ativado');
     inputBusca.classList.toggle('ativado');
+})
+
+linkMenu.addEventListener('click', () => {
+    hamburguer.classList.remove('ativado');
+    Menu.classList.remove('ativado');
+    inputBusca.classList.remove('ativado');
 })
