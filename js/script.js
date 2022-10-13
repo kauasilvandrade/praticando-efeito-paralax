cf931ab2b1b0d18a7f0aa3d58de8e26e2870f80b
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-13 10:28:01 -0300

    site terminado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 const btnOpen = document.querySelector('.btn-open');
-
 const hamburguer = document.querySelector('.hamburguer')
 const Menu = document.querySelector('.listaDeLinks');
 const inputBusca = document.querySelector('.input-busca');
-const linkMenu = document.querySelectorAll('.link');
 
 function mudouTamanho() {
     if (window.innerWidth <= 968) {
         hamburguer.classList.remove('ativado');
         Menu.classList.remove('ativado');
+        caixaInput.classList.remove('ativado');
         inputBusca.classList.remove('ativado');
     }
 }
@@ -18,19 +17,3 @@ btnOpen.addEventListener('click', () => {
     Menu.classList.toggle('ativado');
     inputBusca.classList.toggle('ativado');
 })
-
-function clickSumirMenu() {
-    if (
-        hamburguer.classList.contains('ativado'), 
-        Menu.classList.contains('ativado'), 
-        inputBusca.classList.contains('ativado')
-    ){
-        hamburguer.classList.remove('ativado');
-        Menu.classList.remove('ativado');
-        inputBusca.classList.remove('ativado');
-    } else {
-        hamburguer.classList.contains('ativado');
-        Menu.classList.contains('ativado');
-        inputBusca.classList.contains('ativado');
-    }
-}
